9716ce1a6c847598ab6c264f1cd29b6a25762f53
Yemezzy <ayubayelmee222@gmail.com>
2024-03-10 22:09:09 +0100

    first commit

---
+++ src/Pages/FIrstpage.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaTelegramPlane, FaYoutube } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGlobe } from "react-icons/fa";
+
+
+export const FIrstpage = () => {
+  return (
+    <div className="">
+      <div className="pb-3 md:px-3 h-[80%] first-page border-white border-b-4">
+        <div className="flex md:hidden h-[130px] shoi  p-5">
+          <section className="md:ml-20 mt-[20%] md:mt-[10%] vvs rounded-full w-[110px] h-[110px] text-center bg-[#142028] border-8 border-white">
+            <p className="pt-[70%] text-white text-xs font-bold">JAGO INU</p>
+          </section>
+        </div>
+        <div className="md:h-[200px] hidden md:block h-[150px] sho  p-5">
+          <div className="flex justify-between">
+            <section className="md:ml-20 mt-[25%] md:mt-[10%] vvs rounded-full w-[150px] h-[150px] text-center bg-[#142028] border-8 border-white">
+              <p className="pt-[75%] text-white font-bold">JAGO INU</p>
+            </section>
+            <section className="ml-7 flex gap-5 text-lg font-semibold items-center text-[#dacb31] md:mt-[15%]  text-center  border-white">
+              <a href="">Home</a>
+              <a href="">About</a>
+              <a href="">Tokenomics</a>
+              <a href="">Whitepaper</a>
+            </section>
+          </div>
+        </div>
+      </div>
+
+      <div className="md:mt-14 py-14 vvv md:p-14 bbn text-xl text-[#e1e1e1]">
+        <p className="md:text-5xl text-2xl font-bold text-center mt-10">
+          JAGO INU Revolutionizing Football Predictions with Blockchain
+        </p>
+        {/* <p>
+          Welcome to JAGO INU, where Holders of token can express their
+          thoughts, make predictions, and engage in the excitement of UEFA
+          Champions League matches like never before.
+        </p>
+        <p className="mt-2">
+          {" "}
+          The project JAGO INU is Inspired by the UEFA Champions League, where
+          holders will earn rewards on every correct predictions being made.
+        </p> */}
+      </div>
+
+      <div className="text-right px-14">
+        <div className="flex gap-5 justify-center  pb-5 text-[#dacb31] ">
+          <a href="">
+            <FaTelegramPlane className="md:text-5xl text-3xl " />
+          </a>
+          <a href="">
+            <FaTwitter className="md:text-5xl text-3xl" />
+          </a>
+          <a href="">
+            <FaYoutube className="md:text-5xl text-3xl" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FIrstpage;
+
---
+++ src/Pages/Rewards.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Rewards = () => {
+  return (
+    <div>
+      <div className="mt-[7%] md:text-2xl text-[#e1e1e1] px-5 md:px-14">
+        <div className="border-[#dacb31] w-full md:w-[70%] border-t-4 md:absolute right-10"></div>
+        <p className="text-[#dacb31] md:text-5xl font-semibold md:py-7 py-5 text-2xl">
+          Rewards
+        </p>
+
+        <p className=" md:mt-5 mt-2">
+          A new token is created at the beginning of every season, the token
+          created will be distributed as reward for every correct predction won.
+        </p>
+        <p className=" mt-2">
+          At the end of the season, we burn the distributed tokens as rewards,
+          while the original JAGO INU token remains active for buying and
+          selling.
+        </p>
+        <p className=" mt-2">
+          But the excitement doesn't end there. The rewards accumulate
+          throughout the season, creating anticipation and engagement among the
+          community.
+        </p>
+
+        <p className=" mt-2 ">
+          By holding JAGO INU tokens, you gain exclusive access to prediction
+          activities and opportunities to earn rewards. It's a token designed by
+          fans, for fans
+        </p>
+        <p className=" mt-2 ">
+          At the end of every season, We will do a giveway to any random holder
+          a ticket to watch the champions league finals live from the stadium.
+        </p>
+
+        <p className="py-3 text-[#dacb31] text-center text-2xl font-semibold">
+          How it works
+        </p>
+        <p>
+          <span className="text-[#dacb31]">Here's how it works:</span> Before
+          each match, the community sets out predictions and rewards for the
+          winning team. Whether it's predicting the winner or the number of
+          goals scored, there's a reward for every correct prediction.
+        </p>
+        <p className="py-3 text-[#dacb31] text-xl font-semibold">Example:</p>
+        <p>Take A vs. B for a match prediction</p>
+        <p className=" mt-2 ">
+          For instance, if Team A wins the match, rewards go to those who
+          predicted A's victory. If the match ends in a draw, only those who
+          chose a draw are rewarded.
+        </p>
+        <p className=" mt-2 ">
+          <span className="text-[#dacb31]">Note:</span> Rewards will
+          be given to only holders of our token(JAGO INU).
+        </p>
+        <div className="border-[#dacb31] w-full mt-7 border-t-4 "></div>
+      </div>
+    </div>
+  );
+}
+
+export default Rewards
